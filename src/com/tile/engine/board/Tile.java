04d690aeaa9812ffc61b744e7cbd88a759837c88
aybarsyildiz <aybarsyildizz@hotmail.com>
2020-10-29 14:52:28 +0300

    hash map ve kare oluşturma methodları

--- src/com/tile/engine/board/Tile.java
+++ src/com/tile/engine/board/Tile.java
@@ -1,12 +1,34 @@
 package com.tile.engine.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.tile.engine.players.Player;
 
 public abstract class Tile {
 
     protected final int tileCoordinate;
 
-    Tile(int tileCoordinate){
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    //hash-map kullanarak board oluşturulması
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles(){
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0; i<400; i++){
+            emptyTileMap.put(i,new EmptyTile(i));
+        }
+
+        return emptyTileMap;
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Player player){
+
+        return player != null ? new AltinTile(tileCoordinate) : EMPTY_TILES.get(tileCoordinate);
+        //eğer oyuncu o karede değilse altın karesi oluşturma methodu
+    }
+
+    private Tile(int tileCoordinate){
         this.tileCoordinate = tileCoordinate;
     }
 
@@ -15,7 +37,7 @@ public abstract class Tile {
     public abstract Player getPlayer();
 
     public static final class EmptyTile extends Tile{
-
+        //altın olmayan boş kare sınıfı
         EmptyTile(final int coordiante){
             super(coordiante);
         }
@@ -34,7 +56,7 @@ public abstract class Tile {
     }
     public static final class AltinTile extends Tile{
         
-        
+        //altın olan kare sınıfı
 
         AltinTile(int tileCoordinate){
             super(tileCoordinate);
